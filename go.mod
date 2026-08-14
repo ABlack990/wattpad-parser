3dfa6abda0aa65dd27db0542db085be4f5f4f2b1
Alex Black <alexatkinsblack@gmail.com>
2026-08-13 21:36:47 -0400

    Initial commit (whoop whoop)

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ABlack990/duplicity-parser
+
+go 1.26.6
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("The only way to begin is by beginning")
+}
