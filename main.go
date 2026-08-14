9dd3f70085d449acb4188bdaca09facd9cd8e6ff
Alex Black <alexatkinsblack@gmail.com>
2026-08-14 19:48:57 -0400

    Add flag support; add -url flag for supplying the url to parse

--- main.go
+++ main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 )
 
-const (
-	// TODO implement this as input to the interface
-	TABLE_OF_CONTENTS_URL = "https://www.wattpad.com/story/138202651-duplicity-h-s"
-)
-
 func main() {
-	// TODO implement "flags" dependency to set command line flags for usage
-	// https://gobyexample.com/command-line-flags
-	// blah
+	flag.Usage = func() {
+		fmt.Fprintln(os.Stderr, "wattpad-parser: fetches and parses a Wattpad story page.")
+		fmt.Fprintln(os.Stderr)
+		fmt.Fprintln(os.Stderr, "Usage:")
+		fmt.Fprintf(os.Stderr, "  %s -url <story-url>\n\n", os.Args[0])
+		fmt.Fprintln(os.Stderr, "Flags:")
+		flag.PrintDefaults()
+	}
 
-	glossaryText, err := fetchRawHtml(TABLE_OF_CONTENTS_URL)
+	url := flag.String("url", "", "wattpad story base page URL (e.g https://www.wattpad.com/story/138202651-duplicity-h-s) (required)")
+	flag.Parse()
+
+	if *url == "" {
+		fmt.Fprintln(os.Stderr, "error: -url flag is required")
+		os.Exit(1)
+	}
+
+	glossaryText, err := fetchRawHtml(*url)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "failed to fetch glossary:", err)
+		os.Exit(1)
 	}
 
 	fmt.Println(glossaryText)
